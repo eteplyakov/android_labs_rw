ab8c099cc7eb42f4e15fc2476270848dfa715fb7
Egor Teplyakov <eteplyakov@ruswizards.com>
2014-02-18 09:10:56 -0430

    fixed launch gallery renamed variables sorted methods

--- IntentsLauncher/src/com/example/intentslauncher/MainActivity.java
+++ IntentsLauncher/src/com/example/intentslauncher/MainActivity.java
@@ -19,25 +19,47 @@ public class MainActivity extends Activity {
 	private static final String LATITUDE = "37.422006";
 	private static final String LONGITUDE = "-122.084095";
 	private static final String BROWSER = "com.android.browser";
-	private static final String VIDEO_ = "android.media.action.VIDEO_CAPTURE";
+	private static final String VIDEO = "android.media.action.VIDEO_CAPTURE";
 	private static final String IMAGE_TYPE = "image/*";
 	private static final String ZOOM = "20";
 	private static final String MAP_LABEL_TEXT = "This place \nLat 37.422006, Lon -122.084095";
 	private static final String PHOTO_NAME = "myPhoto.jpg";
 	private static final int SELECT_PICTURE = 1;
 	private static final int TAKE_PICTURE = 2;
-	private static final String DIR_NAME = "CameraAppDemo";
+	private static final String photoDirectory_NAME = "CameraAppDemo";
 	private static Uri selectedImageUri_;
-	private static File file_;
-	private static File dir_;
+	private static File selectPhoto;
+	private static File photoDirectory_;
 	private ImageView selectedImage_;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		createDirectoryForPictures();
+		photoDirectory_ = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), photoDirectory_NAME);
+		if (!photoDirectory_.exists()) {
+			photoDirectory_.mkdirs();
+		}
 		showPicture();
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == RESULT_OK) {
+			switch (requestCode) {
+			case SELECT_PICTURE:
+				selectedImageUri_ = data.getData();
+				showPicture();
+				break;
+			case TAKE_PICTURE:
+				selectedImageUri_ = Uri.fromFile(selectPhoto);
+				showPicture();
+				break;
+			default:
+				selectedImageUri_ = null;
+				break;
+			}
+		}
 	}
 
 	public void showPicture() {
@@ -57,45 +79,19 @@ public class MainActivity extends Activity {
 		alertDialog.show();
 	}
 
-	public void makeIntentByPackageName(String packageName) {
-		Intent intent;
-		PackageManager manager = getPackageManager();
-		intent = manager.getLaunchIntentForPackage(packageName);
-		try {
-			startActivity(intent);
-		} catch (ActivityNotFoundException ex) {
-			showDialog(getResources().getString(R.string.map_error_message));
-		}
-	}
-
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (resultCode == RESULT_OK) {
-			switch (requestCode) {
-			case SELECT_PICTURE:
-				selectedImageUri_ = data.getData();
-				showPicture();
-				break;
-			case TAKE_PICTURE:
-				selectedImageUri_ = Uri.fromFile(file_);
-				showPicture();
-				break;
-			default:
-				selectedImageUri_ = null;
-				break;
-			}
-		}
-	}
-
 	public void onGalleryButtonClick(View view) {
-		Intent intent = new Intent();
-		intent.setType(IMAGE_TYPE);
-		intent.setAction(Intent.ACTION_VIEW);
+		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"));
 		startActivity(intent);
 	}
 
 	public void onBrowserButtonClick(View view) {
-		makeIntentByPackageName(BROWSER);
+		PackageManager manager = getPackageManager();
+		Intent intent = manager.getLaunchIntentForPackage(BROWSER);
+		try {
+			startActivity(intent);
+		} catch (ActivityNotFoundException ex) {
+			showDialog(getResources().getString(R.string.app_error_message));
+		}
 	}
 
 	public void onMapButtonClick(View view) {
@@ -117,31 +113,23 @@ public class MainActivity extends Activity {
 		intent.setType(IMAGE_TYPE);
 		intent.setAction(Intent.ACTION_GET_CONTENT);
 		startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
-
 	}
 
 	public void onPhotoButtonClick(View view) {
 		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		file_ = new File(dir_, PHOTO_NAME);
-		intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file_));
+		selectPhoto = new File(photoDirectory_, PHOTO_NAME);
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(selectPhoto));
 		startActivityForResult(intent, TAKE_PICTURE);
 	}
 
 	public void onVideoButtonClick(View view) {
-		Intent intent = new Intent(VIDEO_);
+		Intent intent = new Intent(VIDEO);
 		startActivity(intent);
 	}
 
 	public void onAudioButtonClick(View view) {
 		Intent intent = new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION);
 		startActivity(intent);
-	}
-
-	private void createDirectoryForPictures() {
-		dir_ = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), DIR_NAME);
-		if (!dir_.exists()) {
-			dir_.mkdirs();
-		}
 	}
 
 }
