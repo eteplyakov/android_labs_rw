b9b2107f16de31f5995eb96b3796eaa192bc8d13
Egor Teplyakov <eteplyakov@ruswizards.com>
2014-02-25 10:27:42 +0300

    all views founded inside onCreate() method; added static constatnts for extra keys; now third activity visually as first activity

--- ActivityRotator/src/com/example/activityrotator/MainActivity.java
+++ ActivityRotator/src/com/example/activityrotator/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.activityrotator;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
--- ActivityRotator/src/com/example/activityrotator/ThirdActivity.java
+++ ActivityRotator/src/com/example/activityrotator/ThirdActivity.java
@@ -1,45 +1,37 @@
 package com.example.activityrotator;
 
 import android.app.Activity;
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.widget.EditText;
 
 public class ThirdActivity extends Activity {
 
+	private static String EDIT_TEXT_1="first_edit_text";
+	private static String EDIT_TEXT_2="second_edit_text";
+	
+	private EditText editText1_;
+	private EditText editText2_;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_third);
+		editText1_ = (EditText) findViewById(R.id.first_edit_text);
+		editText2_ = (EditText) findViewById(R.id.second_edit_text);
 	}
 
 	@Override
 	public void onSaveInstanceState(Bundle savedInstanceState) {
 		super.onSaveInstanceState(savedInstanceState);
-		EditText editText1 = (EditText) findViewById(R.id.first_edit_text);
-		EditText editText2 = (EditText) findViewById(R.id.second_edit_text);
-		savedInstanceState.putString("first_edit_text", editText1.getText().toString());
-		savedInstanceState.putString("second_edit_text", editText2.getText().toString());
+		savedInstanceState.putString(EDIT_TEXT_1, editText1_.getText().toString());
+		savedInstanceState.putString(EDIT_TEXT_2, editText2_.getText().toString());
 	}
 
 	@Override
 	public void onRestoreInstanceState(Bundle savedInstanceState) {
 		super.onRestoreInstanceState(savedInstanceState);
-		EditText editText1 = (EditText) findViewById(R.id.first_edit_text);
-		EditText editText2 = (EditText) findViewById(R.id.second_edit_text);
-		editText1.setText(savedInstanceState.getString("first_edit_text"));
-		editText2.setText(savedInstanceState.getString("second_edit_text"));
-	}
-
-	@Override
-	public void onConfigurationChanged(Configuration newConfig) {
-		super.onConfigurationChanged(newConfig);
-		if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-			setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		} else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
-			setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		}
+		editText1_.setText(savedInstanceState.getString(EDIT_TEXT_1));
+		editText2_.setText(savedInstanceState.getString(EDIT_TEXT_2));
 	}
 
 }
